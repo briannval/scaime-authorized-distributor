d19416bc8469de428e2beb277b957537ac706646
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-08-10 02:35:22 +0700

    Initialized navbar (TODO: fix alignment)

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="bg-white dark:bg-gray-900">
-    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+    <div class="grid max-w-screen-xl py-8 mx-auto lg:py-16 lg:grid-cols-12 mt-12">
         <div class="mr-auto place-self-center lg:col-span-7">
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-7xl dark:text-white">The Authorized <br/>Scaime Reseller.</h1>
             <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-2xl dark:text-gray-400">The perfect weighing, tests & measurements, and <br /> automated systems for your business.</p>
@@ -12,7 +12,7 @@
                 Contact Sales
             </a> 
         </div>
-        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex px-4">
             <img src="/main-product.jpg" alt="mockup">
         </div>                
     </div>
