c077acb8125bfc1b42f498d01aaca7e9cb727a99
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-08-10 03:09:19 +0700

    Initialized services component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Hero from './components/Hero.vue';
 import Navbar from './components/Navbar.vue';
+import Services from './components/Services.vue';
 </script>
 
 <template>
   <Navbar />
   <Hero />
+  <Services />
 </template>
 
 <style scoped>
