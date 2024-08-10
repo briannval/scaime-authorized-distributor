ef27c2de337b1d4c2112dad87c947b4cd568011b
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-08-11 01:10:16 +0700

    Update navbar and home styling

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="bg-white dark:bg-gray-900">
-        <div class="grid max-w-screen-xl py-8 mx-auto lg:py-16 lg:grid-cols-12 my-24">
+        <div class="grid max-w-screen-xl py-8 mx-auto lg:py-16 lg:grid-cols-12 min-h-[100vh]">
             <div class="mr-auto place-self-center lg:col-span-7">
                 <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-7xl dark:text-white">The Authorized <br/>Scaime Reseller.</h1>
                 <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-2xl dark:text-gray-400">{{ currPhrase }} &nbsp;</p>
@@ -12,7 +12,7 @@
                     Contact Sales
                 </a> 
             </div>
-            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex place-self-center">
                 <img src="/main-product.jpg" alt="mockup" class="w-full h-full object-cover">
             </div>                
         </div>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-<nav class="bg-white border-gray-200 dark:bg-gray-900">
+<nav class="bg-white border-gray-200 dark:bg-gray-900 fixed w-full top-0 start-0">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SCAIME.</span>
