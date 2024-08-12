e6f00e544a6289cadc2f03fae5bc183627387d15
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-08-12 21:00:34 +0700

    Added highlighting to catalogue on hover

--- src/components/Catalogue.vue
+++ src/components/Catalogue.vue
@@ -18,7 +18,7 @@
           class="w-full text-md text-left rtl:text-right text-gray-500 dark:text-gray-400"
         >
           <thead
-            class="text-md text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+            class="text-lg text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400"
           >
             <tr>
               <th scope="col" class="px-6 py-3" v-for="column in columns">
@@ -27,13 +27,10 @@
             </tr>
           </thead>
           <tbody>
-            <tr
-              class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-              v-for="item in data"
-            >
+            <tr class="bg-white border-b hover:bg-gray-50" v-for="item in data">
               <th
                 scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
               >
                 {{ item.product }}
               </th>
