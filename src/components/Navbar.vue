53bef2ffb1c0c6bf340848e2ab0d0fb2c97a7fde
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-08-15 15:36:33 +0700

    Fixed navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav
-    class="bg-white border-gray-200 dark:bg-gray-900 fixed w-full top-0 start-0 h-16 mx-8 z-10"
+    class="bg-white border-gray-200 dark:bg-gray-900 fixed w-full top-0 start-0 h-16 px-8 z-10"
   >
     <div
       class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-4"
@@ -46,7 +46,7 @@
         id="navbar-default"
       >
         <ul
-          class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+          class="font-medium flex flex-col p-4 md:p-0 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
         >
           <li v-for="link in links">
             <a
