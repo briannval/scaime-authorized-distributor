f7a7bb8cd1667851c952cc7357f38f4248ddcc92
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-08-22 00:50:32 +0700

    Reorganized public structure

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -25,7 +25,7 @@
             class="p-10 md:px-7 xl:px-10 rounded-[20px] bg-white shadow-md hover:shadow-lg mb-8 text-center"
           >
             <img
-              :src="service.image"
+              :src="`/services/${service.image}`"
               alt="Service Image"
               class="w-64 h-64 mx-auto mb-4 object-cover"
             />
@@ -50,31 +50,31 @@ const services = ref([
     title: "Capacity-based solutions",
     description:
       "We offer recommendations for optimum capacity without sacrificing precision and quality.",
-    image: "/capacity.jpg"
+    image: "/capacity.jpg",
   },
   {
     title: "Flexible mounting methods",
     description:
       "Our various mounting kits allow different load cells to be used in diverse scenarios flexibly.",
-    image: "/mounting.jpg"
+    image: "/mounting.jpg",
   },
   {
     title: "Digital and Analogue",
     description:
       "We offer load cells with either digital to analogue output, integrable with our or your own electronics.",
-    image: "/digital.jpg"
+    image: "/digital.jpg",
   },
   {
     title: "Cost saving alternatives",
     description:
       "Utilize our fixed bearings to for horizontal tank weighing to cut costs and increase efficiency.",
-    image: "/cost.png"
+    image: "/cost.png",
   },
   {
     title: "Numerous use cases",
     description:
       "From storage, material supply, manufacturing, packaging, and control, we've got you covered.",
-    image: "/measurement.jpg"
+    image: "/measurement.jpg",
   },
 ]);
 </script>
