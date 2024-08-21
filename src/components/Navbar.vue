509fb1b998a66ce9152f196bf6968684c056f107
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-08-21 22:25:26 +0700

    Navbar fix for mobile

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -48,7 +48,7 @@
         <ul
           class="font-medium flex flex-col p-4 md:p-0 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
         >
-          <li v-for="link in links">
+          <li @click="closeMobile" v-for="link in links">
             <a
               :href="link.href"
               class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:font-bold md:p-0"
@@ -78,6 +78,10 @@ const isMobileOpen = ref(false);
 
 const toggleMobileOpen = () => {
   isMobileOpen.value = !isMobileOpen.value;
+};
+
+const closeMobile = () => {
+  isMobileOpen.value = false;
 };
 </script>
 
